89482d2c666bb01da2bc2e6fb61c73dc6b632a75
Joshuascharley <34540432+joshuacharley@users.noreply.github.com>
2020-08-17 14:12:55 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/storage'
 
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAThhKtb-e4gTbu29DW5LCVwXJAZRcJ-P0",
-    authDomain: "talk-slaone.firebaseapp.com",
-    databaseURL: "https://talk-slaone.firebaseio.com",
+    apiKey: "YOUR AI KEY",
+    authDomain: "/",
+    databaseURL: "/",
     projectId: "talk-slaone",
-    storageBucket: "talk-slaone.appspot.com",
-    messagingSenderId: "1035913102115",
-    appId: "1:1035913102115:web:9805fd1c66b02b800a0296",
-    measurementId: "G-FK7VGYRZNQ"
+    storageBucket: "/",
+    messagingSenderId: "/",
+    appId: "/",
+    measurementId: "/"
   });
 
   const db = firebaseApp.firestore();
